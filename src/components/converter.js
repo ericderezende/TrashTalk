a71923c3df95d22f2b09f9cb9ee120c806a0e77b
ericderezende <ericbrezende@gmail.com>
2020-02-22 11:27:37 -0500

    for logan

--- src/components/converter.js
+++ src/components/converter.js
@@ -5,54 +5,69 @@ var fs = require('fs');
 var axios = require('axios');
 
 const Converter = (props) => {
-    let result, confidence;
-    let outputJSON;
 
-    axios.defaults.headers.common['Content-Type'] = "application/json";
-    axios.defaults.headers.common['Authorization'] = "Bearer "/* Copy token key here */;
-    // function to encode file data to base64 encoded string
-    const base64_encode = (file) =>{
-        // read binary data
-        var bitmap = fs.readFileSync(file);
-        // convert binary data to base64 encoded string
-        return new Buffer(bitmap).toString('base64');
-    }
+    const file = props.setFile('../public/images/HTB1Y.V6aIrrK1Rjy1zeq6xalFXaA.jpg');
 
-    var base = base64_encode('./images/20200201_175818.jpg');
-    axios.post("https://automl.googleapis.com/v1beta1/projects/766644774605/locations/us-central1/models/ICN5802549470285529088:predict",
-        {
-            "payload": {
-                "image": {
-                    "imageBytes": base
-                }
-            }
-        }).then(function(res){
+    // let result, confidence;
+    // let outputJSON;
 
-            if (Object.keys(res.data).length == 0){
-                result = 'trash'
-                confidence = 99;
-            } else{
-                result = res.data.payload[0].displayName;
-                confidence = res.data.payload[0].classification.score;
-                confidence = confidence * 100;
-                confidence = Math.round(confidence);
-            }
+    // axios.defaults.headers.common['Content-Type'] = "application/json";
+    // axios.defaults.headers.common['Authorization'] = "Bearer "/* Copy token key here */;
+    // // function to encode file data to base64 encoded string
+    // const base64_encode = (file) =>{
+    //     // read binary data
+    //     var bitmap = fs.readFileSync(file);
+    //     // convert binary data to base64 encoded string
+    //     return new Buffer(bitmap).toString('base64');
+    // }
 
-            let temp = {
-                "result" : result,
-                "confidence" : confidence
-            };
+    // var base = base64_encode('./images/20200201_175818.jpg');
+    // axios.post("https://automl.googleapis.com/v1beta1/projects/766644774605/locations/us-central1/models/ICN5802549470285529088:predict",
+    //     {
+    //         "payload": {
+    //             "image": {
+    //                 "imageBytes": base
+    //             }
+    //         }
+    //     }).then(function(res){
 
-            temp = JSON.stringify(temp);
+    //         if (Object.keys(res.data).length == 0){
+    //             result = 'trash'
+    //             confidence = 99;
+    //         } else{
+    //             result = res.data.payload[0].displayName;
+    //             confidence = res.data.payload[0].classification.score;
+    //             confidence = confidence * 100;
+    //             confidence = Math.round(confidence);
+    //         }
 
-            outputJSON = JSON.parse(temp);
-            console.log(outputJSON);
-            fs.writeFile('output.json', JSON.stringify(outputJSON), 'utf8', (err, data) => {
-                if (err) console.log(err);
-                else console.log('written');
-        });
-    }).catch(function(err) {
-        console.log(err);
-    });
+    //         let temp = {
+    //             "result" : result,
+    //             "confidence" : confidence
+    //         };
+
+    //         temp = JSON.stringify(temp);
+
+    //         outputJSON = JSON.parse(temp);
+    //         console.log(outputJSON);
+    //         fs.writeFile('output.json', JSON.stringify(outputJSON), 'utf8', (err, data) => {
+    //             if (err) console.log(err);
+    //             else console.log('written');
+    //     });
+    // }).catch(function(err) {
+    //     console.log(err);
+    // });
+
+    return (
+        <div>
+            <b>
+                We Lit!
+            </b>
+            <img src = {file} height="200" width="200">
+
+            </img>
+        </div>
+    );
 
 };
+export default Converter;
